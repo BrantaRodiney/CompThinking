986f151d4da3f2b0703c90f4724bb6817f646d7f
Rodiney Branta <branta.rodiney@gmail.com>
2020-05-01 19:13:21 -0700

    Add content from the step 1 and 2 into a function
    My university, through their online modular system asked us,
    to put the step 1 and 2 (not commited),
    into only one function called "obter_limite", and it should
    return, the available credit the custumer may use.

---
+++ main.py
@@ -0,0 +1,15 @@
+import datetime #informa ano atual - ATP ETAPA2
+
+def obter_limite():
+    cargo_atual = str(input("Para fazermos sua analise de credito, "
+                            "precisamos que voce entre com algumas informacoes:\nCargo atual: "))
+    salario_atual = float(input("Salario Atual: "))
+    ano_de_nascimento = int(input("Ano de nascimento: "))
+    ano_vigente = datetime.date.today()
+    idade_aproximada = (ano_vigente.year - ano_de_nascimento)
+    valor_credito_disponivel = ((salario_atual*(idade_aproximada / 1000)) +100) 
+    print(f"Seu cargo atual: {cargo_atual} \nSeu Salario atual: {salario_atual} \nSeu ano de nascimento: {ano_de_nascimento}")
+    print(f"Voce tem aproximadamente {idade_aproximada} anos.")
+    print(f"Voce possui o valor em credito de R$ {valor_credito_disponivel:.2f}, disponiveis para compras em nossa loja.")
+
+print(obter_limite())
