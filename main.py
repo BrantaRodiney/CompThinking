4e69dbc4e638a174c58fa5d0ead6d1891c9f0a0d
Rodiney Branta <branta.rodiney@gmail.com>
2020-05-02 19:19:13 -0700

    Removido o codigo comentado

--- main.py
+++ main.py
@@ -27,19 +27,3 @@ print(f"Voce tem aproximadamente {idade_aproximada} anos.")
 print(f"Voce possui o valor em credito de R$ {limite:.2f}, disponiveis para compras em nossa loja.")    
 
 time.sleep(5)
-
-# def obter_limite(cargo_atual, salario_atual, ano_de_nascimento, NOME_COMPLETO):
-#     NOME_COMPLETO = "Rodiney Branta"
-#     print(f"Seja bem-vindo a Poppy loja , sou {NOME_COMPLETO}, entre e escolha um produto")
-#     cargo_atual = str(input("Para fazermos sua analise de credito, "
-#                             "solicitamos que voce entre com algumas informacoes:\nCargo atual: "))
-#     salario_atual = float(input("Salario Atual: "))
-#     ano_de_nascimento = int(input("Ano de nascimento: "))
-#     os.system("cls" if os.name == "nt" else "clear")
-#     ano_vigente = datetime.date.today()
-#     idade_aproximada = (ano_vigente.year - ano_de_nascimento)
-#     valor_credito_disponivel = ((salario_atual*(idade_aproximada / 1000)) +100) 
-#     print(f"\nSeu cargo atual: {cargo_atual} \nSeu Salario atual: {salario_atual} \nSeu ano de nascimento: {ano_de_nascimento}")
-#     print(f"Voce tem aproximadamente {idade_aproximada} anos.")
-#     print(f"Voce possui o valor em credito de R$ {valor_credito_disponivel}, disponiveis para compras em nossa loja.")
-#     return valor_credito_disponivel
