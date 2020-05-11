c97d93222576c513e199eab829b745dddc212203
Rodiney Branta <branta.rodiney@gmail.com>
2020-05-11 12:34:29 -0700

    cadastro prod e func obterlimite, verificarproduto
    Código transformado em 2 funções, estas chamadas obter_limite e
    verificar_produto. A primeira verifica os dados basicos iniciais do
    cliente, limite de gastos do cliente; a segunda função valida se o
    produto pode ser comprado, se pode ser aplicado desconto, mostrando o
    resultado com desconto na tela.
    Utilizado o comando while para verificar se o cliente deseja cadastrar
    diversos itens a serem comprados e seus respectivos valores, sendo estes
     calculados através das funções descritas acima.

--- modularizar.py
+++ modularizar.py
@@ -1,61 +1,50 @@
+import datetime
 import os
-import datetime #informa ano atual - ATP ETAPA2
-
-#mostrar e somente mostrar o nome ao usuario 
-
-print("Seja bem-vindo a loja de coelhos, entre e escolha um Rodiney Branta")
-
-Cargo_Atual = str(input("Para fazermos sua analise de credito, precisamos que voce entre com algumas informacoes:\nCargo atual:"))
-Salario_Atual = float(input("Salario Atual:"))
-Ano_Nascimento = int(input("Ano de nascimento:"))
-Ano_Atual = datetime.datetime.today() #ATP_ETAPA2
-Idade_Aproximada = (Ano_Atual.year - Ano_Nascimento) #ATP_ETAPA2
-Credito_Atual = ((Salario_Atual*(Idade_Aproximada / 1000)) +100) #ATP_ETAPA2
-meuNomeCompleto = "Rodiney Branta"
-
-os.system("cls")
-
-print(f"Seu cargo atual: {Cargo_Atual} \nSeu Salario atual: {Salario_Atual} \nSeu ano de nascimento: {Ano_Nascimento}")
-print(f"Voce tem aproximadamente {Idade_Aproximada} anos.") #ATP_ETAPA2
-print(f"Voce possui o valor em credito de R$ {Credito_Atual:.2f}, disponiveis para compras em nossa loja.") #ATP_ETAPA2
-
-#ATP_ETAPA3
-NomeProduto=str(input("Informe o nome de um produto que voce deseja: "))
-ValorProduto=float(input("Informe o valor deste produto: "))
-
-# print("Produto: {} \nValor: R$ {}".format(NomeProduto, ValorProduto))
-
-avaliar = 100 * ValorProduto / Credito_Atual
-
- #Avaliar quando esta liberado ou bloqueado ao parcelar
-if avaliar <= 60:
-     print("LIBERADO")
-elif avaliar <= 90:
-    print("Liberado a parcelar em até 2 vezes")
-elif avaliar <= 100:
-    print("Liberado a parcelar em 3 ou mais vezes")
-else:
-    print("Bloqueado")
 
 
-def ContaCaracter(contador):
-    return len(contador)
+def obter_limite(limpar_tela):
+    NOME_COMPLETO = "Rodiney Branta"
+    print(f"Seja bem-vindo a Poppy loja , sou {NOME_COMPLETO}, entre e escolha um produto\n")
+    cargo_atual = str(input("Para fazermos sua análise de crédito, precisamos que você entre com algumas informações:\nCargo atual:"))
+    salario_atual = float(input("Salario Atual:"))
+    ano_de_nascimento = int(input("Ano de nascimento:"))
+    ano_atual = datetime.datetime.today()
+    idade_aproximada = (ano_atual.year - ano_de_nascimento)
+    credito = ((salario_atual*(idade_aproximada/1000))+100)
+    limpar_tela()
+    print(f"\nSeu cargo atual: {cargo_atual} \nSeu salário atual: {salario_atual} \n"
+            f"Seu ano de nascimento: {ano_de_nascimento}\n"
+            f"Voce possui R$ {credito:.2f} em créditos disponíveis para compras em nossa loja.\n")
+    return credito, idade_aproximada, NOME_COMPLETO
 
 
-def CaracteresPrimeiroNome(meuNomeCompleto):
-    primeiroNome = meuNomeCompleto.find(' ')
-    if primeiroNome != -1:
-        return(primeiroNome)
+def verificar_produto(limite_de_gastos):
+    input("Informe o nome de um produto que você deseja: ")
+    valor_produto=float(input("Informe o valor deste produto: "))
+    avaliar = 100*valor_produto/limite_de_gastos[0]
+    if avaliar <= 60:
+        print("LIBERADO")
+    elif avaliar <= 90:
+        print("Liberado a parcelar em até 2 vezes")
+    elif avaliar <= 100:
+        print("Liberado a parcelar em 3 ou mais vezes")
     else:
-        return(len(meuNomeCompleto))
+        print("Bloqueado")
+    nome_completo_caracteres = int(len(limite[2]))
+    desmembramento_nome = (limite[2]).split(" ", 1)[0]
+    primeiro_nome_caracteres = len(desmembramento_nome)
+    valor_total_com_desconto = float(valor_produto - primeiro_nome_caracteres)
+    if (nome_completo_caracteres <= valor_produto <= limite[1]) or (limite[1] <= valor_produto <= nome_completo_caracteres):
+        print(f"Parabéns, você foi bonificado com um desconto em seu produto, no valor de R$ {float(primeiro_nome_caracteres)}")
+        print(f"O valor do produto com descosto é: R$ {valor_total_com_desconto}")
 
 
-def LiberaDesconto(simNao, valorProduto, meuNomeCompleto):
-    valorTotalComDesconto = float(valorProduto - CaracteresPrimeiroNome(meuNomeCompleto))
-    if  (simNao <= ContaCaracter(meuNomeCompleto)) or (simNao <= Idade_Aproximada):
-        print("Liberado")
-        print(f"Valor produto com desconto: R$ {valorTotalComDesconto}")
+limite = obter_limite(limpar_tela())
 
-print("", ContaCaracter(meuNomeCompleto))
-print("", CaracteresPrimeiroNome(meuNomeCompleto))
-print("", LiberaDesconto(ValorProduto, ValorProduto, meuNomeCompleto))
+conta_produtos_cadastros = int(input("\nInforme quantos produtos você deseja cadastrar: "))
+
+for contador in range(conta_produtos_cadastros):
+    verificar_produto(limite)
+
+print("\nAperte a tecla ENTER para sair")
+input()
